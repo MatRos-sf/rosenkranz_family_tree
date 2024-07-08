8de42e725f5604d723cabe72427a95e44ccd3cd0
MatRos-sf <mateuszrosenkranz@gmail.com>
2024-07-08 21:38:02 +0200

    create member app

---
+++ member/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+
+class GenderChoice(models.IntegerChoices):
+    FEMALE = 1, "Female"
+    MALE = 2, "Male"
+    OTHER = 3, "Other"
+
+
+class Member(models.Model):
+    first_name = models.CharField(max_length=100)
+    middle_name = models.CharField(max_length=100, blank=True, null=True)
+    third_name = models.CharField(max_length=100, blank=True, null=True)
+    surname = models.CharField(max_length=100)
+    gender = models.PositiveSmallIntegerField(choices=GenderChoice.choices)
+
+    born = models.DateField(blank=True, null=True)
+    died = models.DateField(blank=True, null=True)
+
+    description = models.TextField(null=True, blank=True)
+    occupation = models.CharField(null=True, blank=True)
+    country = models.CharField(blank=True, null=True)
+
+    last_modified = models.DateTimeField(auto_now=True)
